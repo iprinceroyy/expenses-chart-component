6243536fc3dfc724f949351c2b9b4f15f66b0a8b
Prince Roy <princeku.ray143@gmail.com>
2022-06-25 19:24:47 +0530

    event handler for popper

--- app.js
+++ app.js
@@ -4,9 +4,26 @@ fetch("data.json")
     .then((Response) => Response.json())
     .then((jsonData) => {
         jsonData.forEach((data) => {
-            console.log(data);
             document.querySelector(
                 `.js-${data.day}`
             ).textContent = `$${data.amount}`;
+
+            const bar = document.querySelector(`.${data.day}`);
+            let clicked = false;
+
+            bar.addEventListener("click", (e) => {
+                const popper = document.querySelector(
+                    `.popper-${e.path[0].classList[1]}`
+                );
+
+                // if bar is once clicked, then hide popper else show
+                clicked
+                    ?
+                    (popper.style.visibility = "hidden") :
+                    (popper.style.visibility = "visible");
+
+                // Change state of clicked
+                clicked = !clicked;
+            });
         });
     });
